aebb3dc0cfb9844bfbc60074c0d613bd38258f65
manlio <manpuglisi94@gmail.com>
2020-01-09 16:48:08 +0100

    refactory

--- sources/filemanagementservice/src/main/java/com/localhostgang/unict/filemanagementservice/FilemanagementserviceApplication.java
+++ sources/filemanagementservice/src/main/java/com/localhostgang/unict/filemanagementservice/FilemanagementserviceApplication.java
@@ -1,13 +1,13 @@
-package com.localhostgang.unict.dsp.objectstorageadapter;
+package com.localhostgang.unict.filemanagementservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ObjectstorageadapterApplication {
+public class FilemanagementserviceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ObjectstorageadapterApplication.class, args);
+		SpringApplication.run(FilemanagementserviceApplication.class, args);
 	}
 
 }
--- sources/filemanagementservice/src/test/java/com/localhostgang/unict/filemanagementservice/FilemanagementserviceApplicationTests.java
+++ sources/filemanagementservice/src/test/java/com/localhostgang/unict/filemanagementservice/FilemanagementserviceApplicationTests.java
@@ -1,13 +1,13 @@
-package com.localhostgang.unict.dsp.objectstorageadapter;
+package com.localhostgang.unict.filemanagementservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ObjectstorageadapterApplicationTests {
+class FilemanagementserviceApplicationTests {
 /*
 	@Test
 	void contextLoads() {
-	} */
-
+	}
+*/
 }
