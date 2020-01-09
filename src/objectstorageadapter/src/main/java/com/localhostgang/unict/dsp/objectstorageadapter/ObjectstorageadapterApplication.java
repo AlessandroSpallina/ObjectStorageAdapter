217aa5c0793d4037a770e2c60dbc6be1068c9697
manlio <manpuglisi94@gmail.com>
2020-01-09 12:23:42 +0100

    java springboot inizializer push

---
+++ src/objectstorageadapter/src/main/java/com/localhostgang/unict/dsp/objectstorageadapter/ObjectstorageadapterApplication.java
@@ -0,0 +1,13 @@
+package com.localhostgang.unict.dsp.objectstorageadapter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObjectstorageadapterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObjectstorageadapterApplication.class, args);
+	}
+
+}
