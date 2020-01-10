684e2bdb18d56ec493249f38b70610fdd968c8f7
AlessandroSpallina <alessandrospallina1@gmail.com>
2020-01-10 21:28:50 +0100

    added security dependency

--- sources/filemanagementservice/src/main/java/com/localhostgang/unict/filemanagementservice/controller/FileManagementController.java
+++ sources/filemanagementservice/src/main/java/com/localhostgang/unict/filemanagementservice/controller/FileManagementController.java
@@ -3,19 +3,16 @@ package com.localhostgang.unict.filemanagementservice.controller;
 import com.localhostgang.unict.filemanagementservice.entity.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/")
 public class FileManagementController {
     @Autowired
     private UserRepository repository;
 
-    /*@GetMapping("/")
-    public String getFiles() {
-        return "ciao";
-    }*/
-
-    @GetMapping("/ping")
+    @GetMapping("ping")
     public String ping() {
         return "pong";
     }
