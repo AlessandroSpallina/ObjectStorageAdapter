8e54258ca7f63fda4473bfe8b260b46277afbf66
manlio <manpuglisi94@gmail.com>
2020-01-13 15:39:11 +0100

    modified apigateway to Zuul

--- sources/apigateway/src/main/java/com/locahostgang/unict/apigateway/ApigatewayApplication.java
+++ sources/apigateway/src/main/java/com/locahostgang/unict/apigateway/ApigatewayApplication.java
@@ -1,9 +1,11 @@
-package com.localhostgang.unict.apigateway;
+package com.locahostgang.unict.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableZuulProxy
 public class ApigatewayApplication {
 
 	public static void main(String[] args) {
--- sources/apigateway/src/test/java/com/locahostgang/unict/apigateway/ApigatewayApplicationTests.java
+++ sources/apigateway/src/test/java/com/locahostgang/unict/apigateway/ApigatewayApplicationTests.java
@@ -1,4 +1,4 @@
-package com.localhostgang.unict.apigateway;
+package com.locahostgang.unict.apigateway;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
