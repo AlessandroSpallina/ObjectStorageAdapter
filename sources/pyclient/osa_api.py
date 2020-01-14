24c8b15e07f4f84fbcda942e7b28fdb99655e58a
AlessandroSpallina <alessandrospallina1@gmail.com>
2020-01-14 21:33:23 +0100

    metrics in api lib done

--- sources/pyclient/osa_api.py
+++ sources/pyclient/osa_api.py
@@ -1,46 +1,68 @@
+import os
 import requests
 from requests.auth import HTTPBasicAuth
 import json
+import csv
 
 class ObjectStorageAdapterAPI:
     def __init__(self, user, pasw, endpoint):
         self.user = user
         self.pasw = pasw
         self.endpoint = endpoint
+        # used to store metrics, for each request:
+        # http method, payload size input, payload size output, response time (in sec), http status code
+        self.metrics = []
 
     def _url(self, path):
         return self.endpoint + path
 
+    def osa_metrics_to_csv(self, path):
+        with open(path, 'w', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow(['Method', 'Payload Size In', 'Payload Size Out', 'Response Time', 'Status Code'])
+            writer.writerows(self.metrics)
+
     def ping(self):
         headers = {'Content-Type':'application/json'}
-        return requests.get(self._url('/ping'), headers=headers, auth=(self.user, self.pasw))
-
+        resp = requests.get(self._url('/ping'), headers=headers, auth=(self.user, self.pasw))
+        self.metrics.append(['GET /ping', 0, len(resp.content), resp.elapsed.total_seconds(), resp.status_code])
+        return resp
 
     # GET 1
     def get_file_url(self, file_id):
         headers = {'Content-Type':'application/json'}
-        return requests.get(self._url('/{}'.format(file_id)), headers=headers, auth=(self.user, self.pasw))
+        resp = requests.get(self._url('/{}'.format(file_id)), headers=headers, auth=(self.user, self.pasw))
+        self.metrics.append(['GET /{}'.format(file_id), 0, len(resp.content), resp.elapsed.total_seconds(), resp.status_code])
+        return resp
 
     # GET 2
     def get_file_list(self):
         headers = {'Content-Type':'application/json'}
-        return requests.get(self._url('/'), headers=headers, auth=(self.user, self.pasw))
+        resp = requests.get(self._url('/'), headers=headers, auth=(self.user, self.pasw))
+        self.metrics.append(['GET /', 0, len(resp.content), resp.elapsed.total_seconds(), resp.status_code])
+        return resp
 
     # POST 3
     def send_metadata_file(self, filename, author):
         headers = {'Content-Type':'application/json'}
-        payload = {'filename':filename, 'author':author}
-        return requests.post(self._url('/'), headers=headers, data=json.dumps(payload), auth=(self.user, self.pasw))
+        payload = json.dumps({'filename':filename, 'author':author})
+        resp = requests.post(self._url('/'), headers=headers, data=payload, auth=(self.user, self.pasw))
+        self.metrics.append(['POST /', len(payload), len(resp.content), resp.elapsed.total_seconds(), resp.status_code])
+        return resp
 
     # POST 4
     def send_file(self, file_id, path):
         files = [('file', open(path, 'rb'))]
-        return requests.post(self._url('/{}'.format(file_id)), files=files, auth=(self.user, self.pasw))
+        resp = requests.post(self._url('/{}'.format(file_id)), files=files, auth=(self.user, self.pasw))
+        self.metrics.append(['POST /{}'.format(file_id), os.fstat(files[0][1].fileno()).st_size, len(resp.content), resp.elapsed.total_seconds(), resp.status_code])
+        return resp
 
     # DELETE 5
     def delete_file(self, file_id):
         headers = {'Content-Type':'application/json'}
-        return requests.delete(self._url('/{}'.format(file_id)), headers=headers, auth=(self.user, self.pasw))
+        resp = requests.delete(self._url('/{}'.format(file_id)), headers=headers, auth=(self.user, self.pasw))
+        self.metrics.append(['DELETE /{}'.format(file_id), 0, len(resp.content), resp.elapsed.total_seconds(), resp.status_code])
+        return resp
 
     # POST 6 [@findme] : not implemented
     # def register_user(self):
--- sources/pyclient/stresser.py
+++ sources/pyclient/stresser.py
@@ -1,31 +1,10 @@
+import os
 from osa_api import ObjectStorageAdapterAPI
 
-def load1():
-    pass
-
-def load2():
-    pass
-
-def load3():
-    pass
-
-def load4():
-    pass
-
-def export_csv():
-    pass
-
-
-
-
-
-
-def main():
-    osa = ObjectStorageAdapterAPI('user@a.a', 'user', 'http://151.97.150.179:9090/fms')
-
-    print("============ STRESSER CLIENT ============")
-    print("Testing API:")
-
+'''
+Call all APIs and print a message based on HTTP status code
+'''
+def api_test_print(osa):
     ret = osa.ping().status_code
     if(ret == 200): # http ok
         print("* Ping: OK")
@@ -62,7 +41,77 @@ def main():
     if(ret == 200): # http ok
         print("* (5) DeleteFile: OK")
     else:
-        print("* (4) DeleteFile: FAIL[{}]".format(ret))
+        print("* (5) DeleteFile: FAIL[{}]".format(ret))
+
+'''
+Load1:
+    *   N1 POST / (metadata)
+    *   N1 POST /{id} (file)
+'''
+def load1(osa, N1, metrics):
+    id_list = []
+
+    for i in range(N1):
+        resp = osa.send_metadata_file("video_{}.mp4".format(i), "lbry inc")
+        if resp.status_code == 200:
+            id_list.append(resp.json()['id'])
+        # log metriche qui
+        #metrics.append({'http_method':'GET', })
+
+    for i in range(len(id_list)):
+        resp = osa.send_file(id_list.pop(), './files/video.mp4')
+        if resp.status_code == 200:
+            pass
+
+
+
+
+
+
+
+
+def load2():
+    pass
+
+def load3():
+    pass
+
+def load4():
+    pass
+
+
+
+
+
+
+
+def main():
+    print("============ STRESSER CLIENT ============")
+
+    osa = ObjectStorageAdapterAPI('user@a.a', 'user', 'http://192.168.1.118:9090/fms')
+
+    print("\n-> Reading ENV")
+    N1 = os.environ.get('N1', 1)
+    N2 = os.environ.get('N2', 1)
+    N3 = os.environ.get('N3', 1)
+    N4 = os.environ.get('N4', 1)
+    P1 = os.environ.get('P1', 1)
+    P2 = os.environ.get('P2', 1)
+    print("* N1: {}\n* N2: {}\n* N3: {}\n* N4: {}\n* P1: {}\n* P2: {}".format(N1, N2, N3, N4, P1, P2))
+
+    print("\n-> Testing API")
+    api_test_print(osa)
+
+
+
+    print("\n-> Load1")
+    load1(osa, N1)
+
+    osa.osa_metrics_to_csv('metrics.csv')
+
+
+
+
 
 
 
