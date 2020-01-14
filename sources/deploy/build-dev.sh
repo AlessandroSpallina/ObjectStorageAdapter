d7cf3f49736286020bb7e173c1f294a97c402ce6
AlessandroSpallina <alessandrospallina1@gmail.com>
2020-01-14 14:59:19 +0100

    update buid-dev.sh

--- sources/deploy/build-dev.sh
+++ sources/deploy/build-dev.sh
@@ -1,8 +1,16 @@
 docker container prune
 # docker volume prune
+
+docker rmi fms:v0
+docker rmi apigw:v0
+
 cd ../filemanagementservice
+#mvn package
 docker build -t fms:v0 . -f Dockerfile-dev
+
 cd ../apigateway
+#mvn package
 docker build -t apigw:v0 . -f Dockerfile-dev
+
 cd ../deploy
 docker-compose up
