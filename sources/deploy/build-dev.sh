07e72d09711c008228c19162950f37cc0d52de50
AlessandroSpallina <alessandrospallina1@gmail.com>
2020-01-19 00:30:07 +0100

    added build-prod.sh

--- sources/deploy/build-dev.sh
+++ sources/deploy/build-dev.sh
@@ -1,20 +1,18 @@
-docker container prune
+#docker container prune
 # docker volume prune
 
-docker rmi fms:v0
-docker rmi apigw:v0
-docker rmi minio-custom:v0
+#docker rmi fms:v0
+#docker rmi apigw:v0
+#docker rmi minio-custom:v0
 
 cd ../filemanagementservice
-#mvn package
 docker build -t fms:v0 . -f Dockerfile-dev
 
 cd ../apigateway
-#mvn package
 docker build -t apigw:v0 . -f Dockerfile-dev
 
 cd ../minio
 docker build -t minio-custom:v0 .
 
-cd ../deploy
-docker-compose up
+#cd ../deploy
+#docker-compose up
