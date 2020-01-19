ee7884fad715622d13a88d031b4b67a389f98dc6
AlessandroSpallina <alessandrospallina1@gmail.com>
2020-01-19 23:59:22 +0100

    directory structure cleaning

--- sources/deploy/build-prod.sh
+++
@@ -1,8 +0,0 @@
-cd ../filemanagementservice
-docker build -t fms-prod:v0 . -f Dockerfile-prod
-
-cd ../apigateway
-docker build -t apigw-prod:v0 . -f Dockerfile-prod
-
-cd ../minio
-docker build -t minio-custom:v0 .
