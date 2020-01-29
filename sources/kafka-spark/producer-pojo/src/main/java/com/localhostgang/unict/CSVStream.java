8177d8574592947e5bb4853c2538bec3b0e85571
manlio <manpuglisi94@gmail.com>
2020-01-29 11:20:40 +0100

    last fix to kafka producer

--- sources/kafka-spark/producer-pojo/src/main/java/com/localhostgang/unict/CSVStream.java
+++ sources/kafka-spark/producer-pojo/src/main/java/com/localhostgang/unict/CSVStream.java
@@ -12,7 +12,7 @@ public class CSVStream {
     private String csvSplitBy = ",";
     private BufferedReader br = new BufferedReader(new FileReader(prop.getProperty("consumer.filepath")));
     private File file = new File(prop.getProperty("consumer.filepath"));
-
+    private int counter = 0;
     private KProducer kp = new KProducer();
 
     public CSVStream() throws FileNotFoundException {
@@ -25,12 +25,11 @@ public class CSVStream {
                 String line;
 
                 br.readLine(); //questo legge la prima riga, che non mi serve a un gazzo
-
                 while((line = br.readLine()) != null) {
                     String[] metrics = line.split(csvSplitBy);
-                    line = metrics[0] + " " + metrics[1] + " " + metrics[2] + " " + metrics[3];
+                    line = counter + " " + metrics[0] + " " + metrics[1] + " " + metrics[2] + " " + metrics[3];
                     kp.produceData(line);
-
+                    counter ++;
                     // aggiunto dopo suggerimento Di Maria
                     Thread.sleep(3000);
                 }
