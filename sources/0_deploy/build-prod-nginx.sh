1e6c2ade1da6006cd0fb3836bbc11750459f8042
AlessandroSpallina <alessandrospallina1@gmail.com>
2020-01-30 20:58:38 +0100

    k8s stuff

--- sources/0_deploy/build-prod-nginx.sh
+++ sources/0_deploy/build-prod-nginx.sh
@@ -3,3 +3,6 @@ docker build --rm -t fms-prod:v0 . -f Dockerfile-prod
 
 cd ../nginx
 docker build --rm -t apigw-nginx:v0 . -f Dockerfile
+
+cd ../kafka-spark/producer-pojo
+docker build --rm -t kafka-producer-prod:v0 . -f Dockerfile-prod
--- sources/0_deploy/k8s/production-nginx/create-config-maps.sh
+++ sources/0_deploy/k8s/production-nginx/create-config-maps.sh
@@ -19,3 +19,8 @@ kubectl get configmap osa-storage-env-file -o yaml > ./storage/osa-storage-confi
 
 kubectl create secret generic osa-storage-secret-file --from-env-file=./storage/storage.secrets --save-config
 kubectl get secret osa-storage-secret-file -o yaml > ./storage/osa-storage-secret-file.yml
+
+# ---
+
+kubectl create configmap kafka-producer-pojo-env-file --from-env-file=./producer-pojo/producer-pojo.properties --save-config
+kubectl get configmap kafka-producer-pojo-env-file -o yaml > ./producer-pojo/kafka-producer-pojo-configmap.yml
